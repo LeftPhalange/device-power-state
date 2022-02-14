4f474eceb39f00543a283274b5592d6d126f351f
Ethan Bovard <ethanbovard@hotmail.com>
2022-02-14 00:22:37 -0500

    Add project files.

---
+++ device-power-state/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.ComponentModel;
+
+namespace device_power_state
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("device_power_state (arguments [both are required]: deviceId, timeoutInSeconds)");
+                return;
+            }
+            var deviceId = args[0];
+            var secs = int.Parse(args[1]);
+            var currentState = SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline;
+            
+            /* Start loop to detect power line status and toggle device status */
+            while (true)
+            {
+                bool isRunningOnBattery = (SystemInformation.PowerStatus.PowerLineStatus == PowerLineStatus.Offline);
+                
+                // If current state is the same at the next iteration, leave it alone
+                if (currentState == isRunningOnBattery)
+                {
+                    Thread.Sleep(secs);
+                    continue;
+                }
+
+                // Toggle device based on PowerLineStatus
+                AppendText($"Is charging: {SystemInformation.PowerStatus.PowerLineStatus}");
+                currentState = ToggleDevice(deviceId, isRunningOnBattery);
+
+                Thread.Sleep(TimeSpan.FromSeconds(secs));
+            }
+        }
+
+        static void AppendText(string Text)
+           => Console.WriteLine(string.Format("[{0} {1}] {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString(), Text));
+
+        static bool ToggleDevice(string hardwareId, bool enableDevice)
+        {
+            string argument = enableDevice ? "/enable-device" : "/disable-device";
+            AppendText($"{(enableDevice ? $"Enabling device {hardwareId}." : $"Disabling device {hardwareId}.")}");
+            try
+            {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = "pnputil",
+                    Arguments = $"{argument} \"{hardwareId}\"",
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                });
+            }
+            catch (Win32Exception ex)
+            {
+                AppendText($"Exception found: {ex.Message}");
+            }
+            return enableDevice;
+        }
+    }
+}
---
+++ device-power-state/README.md
@@ -0,0 +1,18 @@
+﻿# device-power-state
+
+This is a simple .NET Framework 4.0/C# application that disables or enables a device driver, given a hardware instance ID from PnPUtil, in Windows based on the power state. Useful for disabling integrated graphics when running on A/C power to leverage discrete graphics fully.
+
+## How it works
+* This application detects the power state (SystemInformation.PowerStatus.PowerLineStatus) every X seconds.
+* If the PowerLineStatus is Offline (or is on battery power), it will enable or re-enable the specified device driver.
+* Conversely, if the PowerLineStatus is Online (or is on A/C power), it will disable the specified device driver.
+
+## Syntax (not including brackets, both required):
+`device-power-state [deviceId] [timeoutInSeconds]`
+* deviceId: The Instance ID found in PnPUtil when running this command: `pnputil /enum-devices /connected`
+* timeoutInSeconds: How long the cycles should be in between detecting the power state, measured in seconds
+
+## Tested on
+* Windows 10
+* Windows 11
+* Visual Studio Community 2019
